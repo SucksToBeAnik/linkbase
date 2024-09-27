9e19d6712bddce871c3aa9cde8ca9fbc1a335ada
Al Jami Islam Anik <anik.islam1494@gmail.com>
2024-09-27 18:08:14 +0600

    solving issues

--- backend/db/config.py
+++ backend/db/config.py
@@ -2,7 +2,13 @@ from sqlmodel import create_engine, Session, SQLModel
 from settings import env_store
 
 database_url = env_store.DATABASE_URL
+username = env_store.POSTGRES_USER
+password = env_store.POSTGRES_PASSWORD
+db = env_store.POSTGRES_DB
 print(f"-----${database_url}-----")
+print(f"-----${username}-----")
+print(f"-----${password}-----")
+print(f"-----${db}-----")
 
 engine = create_engine(database_url)
 
